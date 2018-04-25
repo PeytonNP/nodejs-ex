d36146f5da4770e2df6c6be049695e2259697283
PeytonNP <peytonpuckett@pal-nat186-59-111.itap.purdue.edu>
2018-04-25 08:50:38 -0400

    checking if controller works

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   res.render('index.html');
@@ -24,8 +24,14 @@ app.get('/', function (req, res) {
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
-});
+});*/
 
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
+
+controller(app);
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
